a15b989efacb2d07613940992fae931559cadf2f
Nico <nicodiazan@gmail.com>
2022-07-05 19:31:01 +0200

    Refactor javascript code

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-const wrapper = document.querySelector("main");
-const calculator = wrapper;//wrapper.querySelector("main");
+const calculator = document.querySelector("main");
+
+function setup() {
+
+    calculator.addEventListener("mousedown", ()=>{
+        calculator.classList.add("active");
+        calculator.addEventListener("mousemove", onDrag);
+    });
+    document.addEventListener("mouseup", ()=>{
+        calculator.classList.remove("active");
+        calculator.removeEventListener("mousemove", onDrag);
+    });
+}
+
 function onDrag({movementX, movementY}){
-    let getStyle = window.getComputedStyle(wrapper);
+    let getStyle = window.getComputedStyle(calculator);
     let leftVal = parseInt(getStyle.left);
     let topVal = parseInt(getStyle.top);
-    wrapper.style.left = `${leftVal + movementX}px`;
-    wrapper.style.top = `${topVal + movementY}px`;
+    calculator.style.left = `${leftVal + movementX}px`;
+    calculator.style.top = `${topVal + movementY}px`;
 }
-calculator.addEventListener("mousedown", ()=>{
-    calculator.classList.add("active");
-    calculator.addEventListener("mousemove", onDrag);
-});
-document.addEventListener("mouseup", ()=>{
-    calculator.classList.remove("active");
-    calculator.removeEventListener("mousemove", onDrag);
-});
+
+setup();
