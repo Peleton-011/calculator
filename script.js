d679167d45ac5bd427e3ef7d5d400c997f0781f9
Nico <nicodiazan@gmail.com>
2022-07-06 11:25:32 +0200

    Add functionality to number buttons

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let display = "0";
+let displayText = "0";
+const display = document.querySelector(".display");
 //current operation to be performed
 //0 -> None, awaiting operation
 //1 -> Subtraction
@@ -10,49 +11,57 @@ let currOperation = 0;
 const calculator = document.querySelector("main");
 
 function setup() {
+    //All number buttons
+
+    const numBtns = document.querySelectorAll(".num");
+    numBtns.forEach((numBtn) => {
+        numBtn.addEventListener("click", (e) => {
+            updateDisplay(Number(numBtn.textContent));
+        });
+    });
 
     //Equals button
     const eqlBtn = document.querySelector(".equals");
-    eqlBtn.addEventListener("click", e => {
+    eqlBtn.addEventListener("click", (e) => {
         operate();
     });
 
     //Divide button
     const divBtn = document.querySelector(".divide");
-    divBtn.addEventListener("click", e => {
+    divBtn.addEventListener("click", (e) => {
         changeOperation(3);
     });
 
     //Multiply button
     const mltBtn = document.querySelector(".multiply");
-    mltBtn.addEventListener("click", e => {
+    mltBtn.addEventListener("click", (e) => {
         changeOperation(4);
     });
 
     //Subtract button
     const subBtn = document.querySelector(".subtract");
-    subBtn.addEventListener("click", e => {
+    subBtn.addEventListener("click", (e) => {
         changeOperation(1);
     });
 
     //Add button
     const addBtn = document.querySelector(".add");
-    addBtn.addEventListener("click", e => {
+    addBtn.addEventListener("click", (e) => {
         changeOperation(2);
     });
 
     //Mouse tracking and dragging of the calculator
-    calculator.addEventListener("mousedown", ()=>{
+    calculator.addEventListener("mousedown", () => {
         calculator.classList.add("active");
         calculator.addEventListener("mousemove", onDrag);
     });
-    document.addEventListener("mouseup", ()=>{
+    document.addEventListener("mouseup", () => {
         calculator.classList.remove("active");
         calculator.removeEventListener("mousemove", onDrag);
     });
 }
 
-function onDrag({movementX, movementY}){
+function onDrag({ movementX, movementY }) {
     let getStyle = window.getComputedStyle(calculator);
     let leftVal = parseInt(getStyle.left);
     let topVal = parseInt(getStyle.top);
@@ -61,20 +70,20 @@ function onDrag({movementX, movementY}){
 }
 
 function updateDisplay(num) {
-    if (display == "0") {
-        display = num;
+    if (displayText.length > 9){
         return;
     }
-    display += num;
-    return;
+    displayText = Number(displayText);
+    displayText *= 10;
+    displayText += num;
+    displayText = String(displayText);
+    display.textContent = displayText;
 }
 
 function changeOperation(operation) {
     console.log(operation);
 }
 
-function operate() {
-
-}
+function operate() {}
 
 setup();
